6f10471ef2efb64003f30d0b8a3b15145b13bfd3
Mo Zafar <zafarmo@hotmail.com>
2017-04-11 10:29:54 -0400

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Mozaffar Zafar Mohtashami on 4/11/17.
+//  Copyright © 2017 FairLogic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
